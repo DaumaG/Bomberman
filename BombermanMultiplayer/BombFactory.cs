ac7ea4a33243d2554c9912f80bfa19db6b2064de
skaitam <tam.skaiste@gmail.com>
2019-10-15 17:23:53 +0300

    Builder updated

--- BombermanMultiplayer/BombFactory.cs
+++ BombermanMultiplayer/BombFactory.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BombermanMultiplayer.Builder;
 
 namespace BombermanMultiplayer
 {
@@ -16,7 +17,7 @@ namespace BombermanMultiplayer
         public override GameObject Create(int atLength, int atColumn, int playerNumber)
         {
             Bomb newBomb = 
-                new Bomb.BombBuilder(atLength, atColumn, 8, 48, 48, 48, 48, Convert.ToByte(playerNumber)).addDetonationTime(2000).addPower(3).build();
+                new Bomb.BombBuilder(atLength, atColumn, 8, 48, 48, 48, 48, Convert.ToByte(playerNumber)).AddDetonationTime(new BombDetonation("medium")).AddPower(new BombPower("medium")).Build();
             return newBomb;
         }
     }
---
+++ BombermanMultiplayer/Builder/BombDetonation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BombermanMultiplayer.Builder
+{
+    public class BombDetonation
+    {
+        int detonationTime = 0;
+        public BombDetonation(string time)
+        {
+            switch (time)
+            {
+                case "short":
+                    detonationTime = 1000;
+                    break;
+                case "medium":
+                    detonationTime = 2000;
+                    break;
+                case "long":
+                    detonationTime = 3000;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public int toInt()
+        {
+            return detonationTime;
+        }
+    }
+}
